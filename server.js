8cd75ae144be3129ea92d4e7cc09245b69d68f40
soffar <ahmedamrahmed73@gmail.com>
2025-08-02 18:43:09 +0300

    basic file sturcture

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./database/db");
+
+const app = express();
+const PORT = process.env.PORT;
+
+connectDB();
+
+//When a client (like a frontend or API consumer) sends JSON data in the body of an HTTP request
+//(like in POST or PUT requests), this line allows your server to read and access that data.
+//It's a middleware that allows your server to read and access the JSON data sent by the client.
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
